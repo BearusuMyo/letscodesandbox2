47401f44d206bef7388d966d2808f6301e3c6505
Hiago Cardoso de Melo <hiago.go.mobile@gmail.com>
2022-02-09 21:54:35 -0400

    Initial Commit

---
+++ js/aula01.js
@@ -0,0 +1,21 @@
+/*escola = 1;
+escola = "texto";
+escola = "lets code";
+
+Sintaxe 
+let chave = valor;*/
+
+var escola = ['Let\'s Code', 'Cloud Skills Boost']
+let nome = "Hiago Cardoso de Melo"
+let studying = true;
+let working = true;
+
+/*
+Executar tareas
+Sintaxe
+comando();
+comando(parameter); 
+alert("GUd KNitE");
+*/
+
+prompt("Tell me who you aaaaaaare:\n", "Don't lie~ uwu");
---
+++ js/program.js
@@ -0,0 +1,16 @@
+//ECMASCRIPT 5
+//Storing data
+let nome = prompt("Escreva seu nome: ");
+let email = prompt("Escreva seu email: ");
+let telefone = prompt("Escreva seu telefone: ");
+
+
+// Running 🏃💨 (dev)
+
+console.log(nome);
+console.log(email);
+console.log(telefone);
+
+document.write("Nome: ",nome,"<br>");
+document.write("Email: ",email,"<br>");
+document.write("Telefone: ",telefone,"<br><hr>");
