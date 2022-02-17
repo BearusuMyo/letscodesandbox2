9dc1301b421691b57348828743b0e9a76c77f0f2
Hiago Cardoso de Melo <hiago.go.mobile@gmail.com>
2022-02-16 21:30:01 -0400

    Updated to exercise from JS (Conditional Structs.)

--- aula02/js/edade.js
+++ aula02/js/edade.js
@@ -1,16 +1,19 @@
 function analisar(){
-    var idade = parseFloat(document.getElementById("age").value);
+    var idade = parseInt(document.getElementById("age").value);
     var resulta = "";
+    while (!idade || idade < 0){
+        alert ("Ponha um número válido, bobão! >:O");
+        idade = parseInt(prompt("Informe agora!"));
+        document.getElementById("age").value = idade;
+    }
     if (idade < 18){
         resulta = "Baby";
     } else if (idade < 60 && idade >= 18){
         resulta = "Adulto(a)";
     } else if (idade >=60 && idade < 120){
         resulta = "E-doso(a)";
-    } else if (idade != NaN){
-        resulta = "Defunto(a) e esqueçeram de enterrar!";
     } else {
-        resulta = "Informe um número, bobão! >:O";
+        resulta = "Defunto(a) e esqueçeram de enterrar!";
     }
     document.getElementById("result2").value = resulta;
 }
